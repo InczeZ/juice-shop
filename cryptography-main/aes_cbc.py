a7433c13b214d5aa82ea6d159ef79533bc6b5af8
InczeZ <z.incze2003@gmail.com>
2025-05-13 16:38:33 +0300

    lab4_2

--- cryptography-main/aes_cbc.py
+++ cryptography-main/aes_cbc.py
@@ -93,34 +93,33 @@ if operation == 'enc':
 
 # decryption
 else:
-    print('Decrypting... (not implemented yet)', end='')
+    print('Decrypting...', end='')
 
-    # # read the salt, the IV, and the ciphertext from the input file
-    # with open(ifile_name, 'rb') as f:
-    #     salt = f.read(16)
-    #     iv = f.read(16)
-    #     ciphertext = f.read()
+    # read the salt, the IV, and the ciphertext from the input file
+    with open(ifile_name, 'rb') as f:
+        salt = f.read(16)
+        iv = f.read(16)
+        ciphertext = f.read()
 
-    # # derive the 32-byte key from the passphrase
-    # # using PBKDF2 with the salt and iteration count 100000
-    # # TODO: key = ...
+    # derive the 32-byte key from the passphrase
+    # using PBKDF2 with the salt and iteration count 100000
+    key = KDF.PBKDF2(passphrase, salt, count=100000, dkLen=32)
+    # create an AES-CBC cipher object for decrypting the ciphertext
+    aes_cbc = AES.new(key, AES.MODE_CBC, iv)
 
-    # # create an AES-CBC cipher object for decrypting the ciphertext
-    # # TODO: aes_cbc = ...
+    # decrypt the ciphertext and remove padding
+    try:
+        padded_plaintext = aes_cbc.decrypt(ciphertext)
+        plaintext = Padding.unpad(
+            padded_plaintext, AES.block_size, style='pkcs7')
+    except ValueError as e:
+        print('Error: Decryption failed.', e)
+        sys.exit(1)
 
-    # # decrypt the ciphertext and remove padding
-    # try:
-    #     # TODO: padded_plaintext = __.decrypt(__)
-    #     # TODO: plaintext = __.unpad(__, __, style=__)
-    #     pass  # TODO: remove this line!
-    # except ValueError:
-    #     print('Error: Decryption failed.')
-    #     sys.exit(1)
+    # write out the plaintext into the output file
+    with open(ofile_name, "wb") as f:
+        f.write(plaintext)
 
-    # # write out the plaintext into the output file
-    # with open(ofile_name, "wb") as f:
-    #     f.write(plaintext)
-
-    # print('Done.')
-    # print('Your solution to Challenge 2: ' + plaintext[:16].hex())
-    # print('Hint: The correct solution starts with 537475.')
+    print('Done.')
+    print('Your solution to Challenge 2: ' + plaintext[:16].hex())
+    print('Hint: The correct solution starts with 537475.')
